c563d799c563557cf460881d99ddb2c3f2dfe98b
deadboyccc <ahmed.mohammed.ali9999@gmail.com>
2025-10-04 05:53:25 +0300

    init

---
+++ src/main/kotlin/dev/dead/springdatarestkt/SpringDataRestKtApplication.kt
@@ -0,0 +1,11 @@
+package dev.dead.springdatarestkt
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringDataRestKtApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringDataRestKtApplication>(*args)
+}
---
+++ src/test/kotlin/dev/dead/springdatarestkt/SpringDataRestKtApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.dead.springdatarestkt
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringDataRestKtApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
