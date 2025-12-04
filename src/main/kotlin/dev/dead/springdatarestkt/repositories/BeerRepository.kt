ed8526562ece049ad5ae7860796ff1ab6d1329f8
Ahmed M. Ali <ahmedmohammed199975@gmail.com>
2025-12-04 13:23:27 +0300

    update API base path to /api/v1 and annotate BeerRepository for REST exposure

--- src/main/kotlin/dev/dead/springdatarestkt/repositories/BeerRepository.kt
+++ src/main/kotlin/dev/dead/springdatarestkt/repositories/BeerRepository.kt
@@ -5,8 +5,10 @@ import dev.dead.springdatarestkt.entities.BeerStyle
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.rest.core.annotation.RepositoryRestResource
 import java.util.UUID
 
+@RepositoryRestResource(path = "beer", collectionResourceRel = "beer")
 interface BeerRepository : JpaRepository<Beer, UUID> {
 
     fun findAllByBeerName(beerName: String, pageable: Pageable): Page<Beer>
